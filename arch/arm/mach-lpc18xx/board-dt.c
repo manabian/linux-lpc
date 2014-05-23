f8147a8b592f63f1563883f69db130c61753f40a
Joachim Eastwood <manabian@gmail.com>
2014-05-23 17:37:27 +0200

    ARM: lpc18xx: add basic support for NXP LPC18xx/43xx SoCs
    Add support for NXP's LPC18xx (Cortex-M3) and LPC43xx (Cortex-M4)
    SoCs. These SoCs are NXP's high preformance MCU line and can run at
    clock speeds up to 180 MHz for LPC18xx and 204 MHz for LPC43xx.
    
    LPC43xx is more or less a LPC18xx with a Cortex-M4F core and a few
    extra peripherals. The LPC43xx series also features one or two
    Cortex-M0 cores that can be used to offload the main M4 core.
    
    Signed-off-by: Joachim Eastwood <manabian@gmail.com>

---
+++ arch/arm/mach-lpc18xx/board-dt.c
@@ -0,0 +1,24 @@
+/*
+ * Device Tree board file for NXP LPC18xx/43xx
+ *
+ * Copyright (C) 2015 Joachim Eastwood <manabian@gmail.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2. This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#include <linux/kernel.h>
+
+#include <asm/v7m.h>
+#include <asm/mach/arch.h>
+
+static const char *const lpc18xx_43xx_compat[] __initconst = {
+	"ea,lpc4357-developers-kit",
+	NULL
+};
+
+DT_MACHINE_START(LPC18XXDT, "NXP LPC18xx/43xx (Device Tree Support)")
+	.dt_compat = lpc18xx_43xx_compat,
+	.restart = armv7m_restart,
+MACHINE_END
