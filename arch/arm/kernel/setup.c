94a92a6f0d8c93b7f523227edfb8e13912f37771
Joachim Eastwood <manabian@gmail.com>
2014-05-30 21:13:34 +0200

    HACK: Hard code fdt pointer setup.c
    Crappy boot loaders suck.

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -895,6 +895,7 @@ void __init setup_arch(char **cmdline_p)
 	const struct machine_desc *mdesc;
 
 	setup_processor();
+	__atags_pointer = 0x28f00000;
 	mdesc = setup_machine_fdt(__atags_pointer);
 	if (!mdesc)
 		mdesc = setup_machine_tags(__atags_pointer, __machine_arch_type);
