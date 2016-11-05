cb673c962868ef52bd328f89606bb97580108202
Joachim Eastwood <manabian@gmail.com>
2016-11-05 19:41:31 +0100

    pint2 stacked irq chip
    Signed-off-by: Joachim Eastwood <manabian@gmail.com>

--- drivers/irqchip/irq-lpc18xx-gpio-pint.c
+++ drivers/irqchip/irq-lpc18xx-gpio-pint.c
@@ -6,7 +6,6 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
- *
  */
 
 #include <linux/clk.h>
@@ -16,6 +15,7 @@
 #include <linux/of.h>
 #include <linux/of_irq.h>
 #include <linux/platform_device.h>
+#include <linux/spinlock.h>
 
 /* LPC18xx GPIO pin interrupt register offsets */
 #define LPC18XX_GPIO_PINT_ISEL		0x000
@@ -25,107 +25,172 @@
 #define LPC18XX_GPIO_PINT_CIENF		0x018
 #define LPC18XX_GPIO_PINT_IST		0x024
 
-#define PINT_MAX_IRQS			32
+#define LPC18XX_GPIO_PINT_IRQS		8
+#define LPC18XX_GPIO_PINT_IRQ_OFFSET	32
 
 struct lpc18xx_gpio_pint_chip {
 	struct irq_domain *domain;
 	void __iomem	  *base;
 	struct clk	  *clk;
-	unsigned int	  revmap[];
+	spinlock_t	  *lock;
+	unsigned	  irq_offset;
+	u32		  type_cache;
 };
 
-static void lpc18xx_gpio_pint_handler(struct irq_desc *desc)
+static void lpc18xx_gpio_pint_edge_ack(struct irq_data *data)
 {
-	struct lpc18xx_gpio_pint_chip *pint = irq_desc_get_handler_data(desc);
-	unsigned int irq = irq_desc_get_irq(desc);
-	unsigned int hwirq = pint->revmap[irq];
-	unsigned int virq;
+	struct lpc18xx_gpio_pint_chip *pint = data->chip_data;
+	u32 mask = BIT(data->hwirq - pint->irq_offset);
 
-	virq = irq_find_mapping(pint->domain, hwirq);
-	generic_handle_irq(virq);
+	writel(mask, pint->base + LPC18XX_GPIO_PINT_IST);
 }
 
-static void lpc18xx_gpio_pint_edge_mask(struct irq_data *d)
+static void lpc18xx_gpio_pint_mask(struct irq_data *data)
 {
-	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(d);
-	u32 mask = d->mask;
+	struct lpc18xx_gpio_pint_chip *pint = data->chip_data;
+	u32 mask = BIT(data->hwirq - pint->irq_offset);
+	u32 type = irqd_get_trigger_type(data);
 
-	irq_gc_lock(gc);
-	irq_reg_writel(gc, mask, LPC18XX_GPIO_PINT_CIENR);
-	irq_reg_writel(gc, mask, LPC18XX_GPIO_PINT_CIENF);
-	irq_gc_unlock(gc);
+	if (type & IRQ_TYPE_LEVEL_MASK) {
+		writel(mask, pint->base + LPC18XX_GPIO_PINT_SIENR);
+	} else if (type & IRQ_TYPE_EDGE_BOTH) {
+		writel(mask, pint->base + LPC18XX_GPIO_PINT_CIENR);
+		writel(mask, pint->base + LPC18XX_GPIO_PINT_CIENF);
+	}
+
+	irq_chip_mask_parent(data);
 }
 
-static void lpc18xx_gpio_pint_edge_unmask(struct irq_data *d)
+static void lpc18xx_gpio_pint_unmask(struct irq_data *data)
 {
-	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(d);
-	u32 type, mask = d->mask;
+	struct lpc18xx_gpio_pint_chip *pint = data->chip_data;
+	u32 mask = BIT(data->hwirq - pint->irq_offset);
+	u32 type = irqd_get_trigger_type(data);
 
-	irq_gc_lock(gc);
-	type = irqd_get_trigger_type(d);
-	if (type & IRQ_TYPE_EDGE_RISING)
-		irq_reg_writel(gc, mask, LPC18XX_GPIO_PINT_SIENR);
-	if (type & IRQ_TYPE_EDGE_FALLING)
-		irq_reg_writel(gc, mask, LPC18XX_GPIO_PINT_SIENF);
-	irq_gc_unlock(gc);
+	if (type & IRQ_TYPE_LEVEL_MASK) {
+		writel(mask, pint->base + LPC18XX_GPIO_PINT_CIENR);
+	} else {
+		if (type & IRQ_TYPE_EDGE_RISING)
+			writel(mask, pint->base + LPC18XX_GPIO_PINT_SIENR);
+		if (type & IRQ_TYPE_EDGE_FALLING)
+			writel(mask, pint->base + LPC18XX_GPIO_PINT_SIENF);
+	}
+
+	irq_chip_unmask_parent(data);
 }
 
 static int lpc18xx_gpio_pint_type(struct irq_data *data, unsigned int type)
 {
-	struct irq_chip_generic *gc = irq_data_get_irq_chip_data(data);
-	u32 mask = data->mask;
+	struct lpc18xx_gpio_pint_chip *pint = data->chip_data;
+	u32 mask = BIT(data->hwirq - pint->irq_offset);
 
-	irq_gc_lock(gc);
+	spin_lock(pint->lock);
 	if (type & IRQ_TYPE_LEVEL_MASK)
-		gc->type_cache |= mask;
+		pint->type_cache |= mask;
 	else
-		gc->type_cache &= ~mask;
-	irq_reg_writel(gc, gc->type_cache, LPC18XX_GPIO_PINT_ISEL);
+		pint->type_cache &= ~mask;
+	writel(pint->type_cache, pint->base + LPC18XX_GPIO_PINT_ISEL);
 
 	switch (type) {
 	case IRQ_TYPE_LEVEL_HIGH:
-		irq_reg_writel(gc, mask, LPC18XX_GPIO_PINT_SIENF);
+		writel(mask, pint->base + LPC18XX_GPIO_PINT_SIENF);
 		break;
 
 	case IRQ_TYPE_LEVEL_LOW:
-		irq_reg_writel(gc, mask, LPC18XX_GPIO_PINT_CIENF);
+		writel(mask, pint->base + LPC18XX_GPIO_PINT_CIENF);
 		break;
 
-	/* IRQ_TYPE_EDGE_* is set in lpc18xx_gpio_pint_edge_unmask */
+	/* IRQ_TYPE_EDGE_* is set in lpc18xx_gpio_pint_unmask */
 	}
 
 	irqd_set_trigger_type(data, type);
-	irq_setup_alt_chip(data, type);
-	irq_gc_unlock(gc);
+	spin_unlock(pint->lock);
 
 	return 0;
 }
 
-static int lpc18xx_gpio_pint_probe(struct platform_device *pdev)
-{
-	struct device_node *np = pdev->dev.of_node;
-	struct lpc18xx_gpio_pint_chip *pint;
-	struct irq_chip_generic *gc;
-	int irqs[PINT_MAX_IRQS];
-	unsigned int max_virqno;
-	struct resource *regs;
-	int nrirqs;
-	int i, ret;
+static struct irq_chip lpc18xx_gpio_pint_irq_chip = {
+	.name			= "gpio-pint",
+	.irq_ack		= lpc18xx_gpio_pint_edge_ack,
+	.irq_mask		= lpc18xx_gpio_pint_mask,
+	.irq_unmask		= lpc18xx_gpio_pint_unmask,
+	.irq_eoi		= irq_chip_eoi_parent,
+	.irq_set_type		= lpc18xx_gpio_pint_type,
+};
 
-	nrirqs = of_irq_count(np);
-	if (nrirqs > PINT_MAX_IRQS)
+static int lpc18xx_gpio_pint_domain_alloc(struct irq_domain *domain,
+					  unsigned int virq,
+					  unsigned int nr_irqs, void *arg)
+{
+	struct irq_fwspec *fwspec = arg;
+	struct irq_fwspec parent_fwspec;
+	irq_hw_number_t hwirq;
+	int i;
+
+	if (!irq_domain_get_of_node(domain->parent))
 		return -EINVAL;
 
-	max_virqno = 0;
-	for (i = 0; i < nrirqs; i++) {
-		irqs[i] = platform_get_irq(pdev, i);
-		if (max_virqno < irqs[i])
-			max_virqno = irqs[i];
+	if (fwspec->param_count != 2)
+		return -EINVAL;
+
+	hwirq = fwspec->param[0];
+	for (i = 0; i < nr_irqs; i++) {
+		pr_err("%s: hwirq %lu\n", __func__, hwirq);
+		irq_domain_set_hwirq_and_chip(domain, virq + i, hwirq + i,
+					      &lpc18xx_gpio_pint_irq_chip,
+					      domain->host_data);
 	}
 
-	pint = devm_kzalloc(&pdev->dev,
-			    sizeof(*pint) + (sizeof(unsigned int) * max_virqno),
-			    GFP_KERNEL);
+	parent_fwspec.fwnode = domain->parent->fwnode;
+
+	parent_fwspec.param_count = 1;
+	parent_fwspec.param[0] = fwspec->param[0];
+
+	return irq_domain_alloc_irqs_parent(domain, virq, nr_irqs,
+					    &parent_fwspec);
+}
+
+static int lpc18xx_gpio_pint_domain_translate(struct irq_domain *d,
+					      struct irq_fwspec *fwspec,
+					      unsigned long *hwirq,
+					      unsigned int *type)
+{
+	if (WARN_ON(fwspec->param_count < 2))
+		return -EINVAL;
+
+	*hwirq = fwspec->param[0];
+	*type = fwspec->param[1] & IRQ_TYPE_SENSE_MASK;
+
+	return 0;
+}
+
+static const struct irq_domain_ops lpc18xx_gpio_pint_irq_domain_ops = {
+	.translate = lpc18xx_gpio_pint_domain_translate,
+	.alloc = lpc18xx_gpio_pint_domain_alloc,
+	.free = irq_domain_free_irqs_common,
+};
+
+static int lpc18xx_gpio_pint_probe(struct platform_device *pdev)
+{
+	struct device_node *parent, *np = pdev->dev.of_node;
+	struct lpc18xx_gpio_pint_chip *pint;
+	struct irq_domain *domain_parent;
+	struct resource *regs;
+	int ret;
+
+	parent = of_irq_find_parent(np);
+	if (!parent) {
+		dev_err(&pdev->dev, "irq find parent failed\n");
+		return -ENXIO;
+	}
+
+	domain_parent = irq_find_host(parent);
+	if (!domain_parent) {
+		dev_err(&pdev->dev, "interrupt-parent not found\n");
+		return -EINVAL;
+	}
+
+	pint = devm_kzalloc(&pdev->dev, sizeof(*pint), GFP_KERNEL);
 	if (!pint)
 		return -ENOMEM;
 
@@ -133,7 +198,6 @@ static int lpc18xx_gpio_pint_probe(struct platform_device *pdev)
 	pint->base = devm_ioremap_resource(&pdev->dev, regs);
 	if (IS_ERR(pint->base))
 		return PTR_ERR(pint->base);
-
 
 	pint->clk = devm_clk_get(&pdev->dev, NULL);
 	if (IS_ERR(pint->clk)) {
@@ -147,40 +211,8 @@ static int lpc18xx_gpio_pint_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	pint->domain = irq_domain_add_linear(np, nrirqs, &irq_generic_chip_ops,
-					     pint);
-	if (!pint->domain) {
-		dev_err(&pdev->dev, "unable setup irq domain\n");
-		ret = -EINVAL;
-		goto err_domain_add;
-	}
-
-	ret = irq_alloc_domain_generic_chips(pint->domain, nrirqs, 2,
-					     "gpio_pint", handle_edge_irq,
-					     0, 0, 0);
-	if (ret) {
-		dev_err(&pdev->dev, "unable alloc irq domain chips\n");
-		goto err_alloc_domain_gc;
-	}
-
-	gc = irq_get_domain_generic_chip(pint->domain, 0);
-	gc->reg_base = pint->base;
-
-	gc->chip_types[0].type		    = IRQ_TYPE_EDGE_BOTH;
-	gc->chip_types[0].handler	    = handle_edge_irq;
-	gc->chip_types[0].chip.irq_ack	    = irq_gc_ack_set_bit;
-	gc->chip_types[0].chip.irq_mask	    = lpc18xx_gpio_pint_edge_mask;
-	gc->chip_types[0].chip.irq_unmask   = lpc18xx_gpio_pint_edge_unmask;
-	gc->chip_types[0].chip.irq_set_type = lpc18xx_gpio_pint_type;
-	gc->chip_types[0].regs.ack	    = LPC18XX_GPIO_PINT_IST;
-
-	gc->chip_types[1].type		    = IRQ_TYPE_LEVEL_MASK;
-	gc->chip_types[1].handler	    = handle_level_irq;
-	gc->chip_types[1].chip.irq_mask	    = irq_gc_mask_disable_reg;
-	gc->chip_types[1].chip.irq_unmask   = irq_gc_unmask_enable_reg;
-	gc->chip_types[1].chip.irq_set_type = lpc18xx_gpio_pint_type;
-	gc->chip_types[1].regs.enable	    = LPC18XX_GPIO_PINT_SIENR;
-	gc->chip_types[1].regs.disable	    = LPC18XX_GPIO_PINT_CIENR;
+	spin_lock_init(pint->lock);
+	pint->irq_offset = LPC18XX_GPIO_PINT_IRQ_OFFSET;
 
 	/* Disable and clear all interrupts */
 	writel(~0, pint->base + LPC18XX_GPIO_PINT_CIENR);
@@ -188,20 +220,16 @@ static int lpc18xx_gpio_pint_probe(struct platform_device *pdev)
 	writel(0,  pint->base + LPC18XX_GPIO_PINT_ISEL);
 	writel(~0, pint->base + LPC18XX_GPIO_PINT_IST);
 
-	for (i = 0; i < nrirqs; i++) {
-		pint->revmap[irqs[i]] = i;
-		irq_set_chained_handler_and_data(irqs[i],
-						 lpc18xx_gpio_pint_handler,
-						 pint);
+	pint->domain = irq_domain_add_hierarchy(domain_parent, 0,
+					  LPC18XX_GPIO_PINT_IRQS, np,
+					  &lpc18xx_gpio_pint_irq_domain_ops,
+					  pint);
+	if (!pint->domain) {
+		clk_disable_unprepare(pint->clk);
+		return -ENOMEM;
 	}
 
 	return 0;
-
-err_alloc_domain_gc:
-	irq_domain_remove(pint->domain);
-err_domain_add:
-	clk_disable_unprepare(pint->clk);
-	return ret;
 }
 
 static const struct of_device_id lpc18xx_gpio_pint_match[] = {
